5fac5d9c827f995308a71b1fd71b67a44f359a7c
Junior Pacheco <junior.pacheco.356@gmail.com>
2023-11-26 19:19:58 -0500

    added formt and initial structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "World";
+  return (
+    <main>
+      <h1>Computer vision</h1>
+
+      <form>
+        <label>
+          <span>Insert URL or type prompt:</span>
+          <input type="text" />
+        </label>
+        <button>Analyze</button>
+        <button>Generate</button>
+      </form>
+    </main>
+  );
 }
 
 export default App;
